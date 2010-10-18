76a967c407464d299293939a881588e4512436d8
Kris Nuttycombe <kris@hylotech.com>
2010-10-18 10:11:02 -0600

    Empty Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PgRails::Application.config.secret_token = '04b060528aae4f7a7bf1d96bbb908d92a1cf855c5f89c8cf0518859ddfb7c7ebb4d655088d3c86a00daefc258971dd339fd76c99f2adf92a9cee5873fda62a35'
