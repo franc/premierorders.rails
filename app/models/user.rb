edf10b700ecb5cbeb39b019fa9f92029b983372f
Kris Nuttycombe <kris.nuttycombe@gmail.com>
2010-10-31 07:26:40 -0600

    Re-enabled user authentication.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable, :timeoutable, :registerable,
-  #devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
