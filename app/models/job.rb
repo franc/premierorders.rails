ab9c61f601c9bc14e6170b0d932ad932d2c38afd
Kris Nuttycombe <kris.nuttycombe@gmail.com>
2010-12-09 17:12:38 -0700

    Added safe handling for Notes column; prep for CSV impl.

--- app/models/job.rb
+++ app/models/job.rb
@@ -40,7 +40,7 @@ class Job < ActiveRecord::Base
     end
   end
 
-  def add_items_from_dvinci(xml)
+  def decompose_xml(xml)
     doc = REXML::Document.new(xml)
     rows = doc.get_elements("//Row").inject([]) do |rows, row_element|
       rows << row_element.get_elements("Cell/Data").map{|cell| cell.text}
@@ -56,16 +56,23 @@ class Job < ActiveRecord::Base
     missing_columns = ['# of Items in Design', 'Description', 'Part Number'] - labels
     raise FormatException, "Did not find required columns in XML document: #{missing_columns.inspect}" unless missing_columns.empty?
 
-    label_columns = (0...labels.size).zip(labels).inject({}) { |m, l| m[l[1]] = l[0]; m }
+    column_indices = (0...labels.size).zip(labels).inject({}) { |m, l| m[l[1]] = l[0]; m }
 
-    item_rows = data_rows.select{|r| r.size == label_columns.size || r.size == label_columns.size + 1}
-    #if data_rows - item_rows
-    #  flash[:warning] = "It looks like some of your data may not have been imported correctly. Please check the format of the input file."
-    #end
+    min_columns = labels.any? {|l| l =~ /Notes/} ? labels.size - 1 : labels.size
+    item_rows = data_rows.select{|r| r.size >= min_columns}
+
+    [column_indices, item_rows]
+  end 
+
+  def add_items_from_dvinci(file)
+    column_indices, item_rows = case File.extname(file.path)
+      when '.xml' then decompose_xml(file)
+      else raise "Did not recognize file type for #{File.extname(file.path)}"
+    end
 
     item_rows.each_with_index do |row, i|
       logger.info "Processing data row: #{row.inspect}"
-      dvinci_product_id = row[label_columns['Part Number']]
+      dvinci_product_id = row[column_indices['Part Number']]
       product_code_matchdata = dvinci_product_id.match(/(\d{3})\.(\w{3})\.(\w{3})\.(\d{3})\.(\d{2})(\w)/)
       item = if product_code_matchdata
         t1, t2, t3, color_key, t5, t6 = product_code_matchdata.captures
@@ -74,13 +81,20 @@ class Job < ActiveRecord::Base
         Item.find_by_dvinci_id(dvinci_product_id)
       end
 
-      item_quantity = row[label_columns['# of Items in Design']].to_i
+      unlabeled_col_values = ((0...row.size).to_a - column_indices.values).map{|i| row[i]}
+      special_instructions = if column_indices['Notes'] 
+        (unlabeled_col_values << row[column_indices['Notes']]).join("; ")
+      else
+        unlabeled_col_values.join("; ")
+      end
+
+      item_quantity = row[column_indices['# of Items in Design']].to_i
       job_item = if (item.nil?)
         job_items.create(
           :ingest_id => dvinci_product_id,
           :quantity  => item_quantity,
-          :comment   => row[label_columns.size],
-          :unit_price => row[label_columns['Material Charge']].to_f / item_quantity,
+          :comment   => special_instructions,
+          :unit_price => row[column_indices['Material Charge']].to_f / item_quantity,
           :tracking_id => i
         )
       else
@@ -88,8 +102,8 @@ class Job < ActiveRecord::Base
           :item      => item,
           :ingest_id => dvinci_product_id,
           :quantity  => item_quantity,
-          :comment   => row[label_columns.size],
-          :unit_price => row[label_columns['Material Charge']].to_f / item_quantity,
+          :comment   => special_instructions,
+          :unit_price => row[column_indices['Material Charge']].to_f / item_quantity,
           :tracking_id => i
         )
       end
@@ -105,7 +119,7 @@ class Job < ActiveRecord::Base
       ]
 
       # Find the item attributes for the imported columns, standardizing from any non-standard names
-      attribute_labels = labels - ignored_attributes
+      attribute_labels = column_indices.keys - ignored_attributes
       attributes = item.nil? ? {} : attribute_labels.inject({}) do |attr_map, name|
         attr = item.item_attrs.find_by_name(DVINCI_CUSTOM_ATTRIBUTES[name] || name)
         attr_map[name] = attr unless attr.nil?
@@ -119,13 +133,13 @@ class Job < ActiveRecord::Base
           job_item.job_item_attributes.create(
             :attribute => attributes[name],
             :ingest_id => name,
-            :value_str => row[label_columns[name]]
+            :value_str => row[column_indices[name]]
           )
         else
           # otherwise, just attach the information as an opaque key/value pair
           job_item.job_item_attributes.create(
             :ingest_id => name,
-            :value_str => row[label_columns[name]]
+            :value_str => row[column_indices[name]]
           )
         end
       end
