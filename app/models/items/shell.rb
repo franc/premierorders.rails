59247a3bf43f1c417bb2201124636adfa429cf39
Kris Nuttycombe <kris@hylotech.com>
2011-01-11 20:33:02 -0700

    Made shell back panel optional. Fixes https://www.pivotaltracker.com/story/show/8518151

--- app/models/items/shell.rb
+++ app/models/items/shell.rb
@@ -2,6 +2,6 @@ require 'items/shell_components.rb'
 
 class Shell < Item
   def self.component_association_types
-    {:required => [ShellVerticalPanel, ShellHorizontalPanel, ShellBackPanel]}
+    {:required => [ShellVerticalPanel, ShellHorizontalPanel], :optional => [ShellBackPanel]}
   end
 end
