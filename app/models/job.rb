2d0b0c67c5c931f43fc608e6d1df32d489954db6
Kris Nuttycombe <kris.nuttycombe@gmail.com>
2011-01-18 15:31:06 -0700

    Fixed failing transitive destroy of job components.

--- app/models/job.rb
+++ app/models/job.rb
@@ -10,8 +10,8 @@ class Job < ActiveRecord::Base
   belongs_to :customer, :class_name => 'User'
   belongs_to :billing_address, :class_name => 'Address'
   belongs_to :shipping_address, :class_name => 'Address'
-  has_many   :job_items
-  has_many   :job_properties, :extend => Properties::Association
+  has_many   :job_items, :dependent => :destroy
+  has_many   :job_properties, :dependent => :destroy, :extend => Properties::Association
 
   has_attached_file :dvinci_xml
 
