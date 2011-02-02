188ec3a8016b955e814b7fb2f1bda88a30f2bea6
Kris Nuttycombe <kris.nuttycombe@gmail.com>
2011-02-02 06:06:57 +0000

    Corrected ranged property value lookup to include all ranges.

--- app/models/items/surcharges.rb
+++ app/models/items/surcharges.rb
@@ -9,7 +9,7 @@ module Items::Surcharges
 
   def surcharge_exprs(units)
     flat = properties.find_value(SURCHARGE).map{|v| term(v.price)}.to_a
-    ranged = properties.find_value(RANGED_SURCHARGE).map{|v| v.expr(units)}.to_a
+    ranged = properties.find_all_by_descriptor(RANGED_SURCHARGE).map{|v| v.property_values}.flatten.map{|v| v.expr(units)}
 
     flat + ranged
   end
