696906699778ce4e219067a95bc449654ad83dec
Kris Nuttycombe <kris.nuttycombe@gmail.com>
2011-02-25 23:29:56 +0000

    Fixed bad equality test.

--- app/helpers/production_batches_helper.rb
+++ app/helpers/production_batches_helper.rb
@@ -1,12 +1,12 @@
 module ProductionBatchesHelper
   def open_batch_options(selected)
-    opts = if selected.forall{|id| id == -1}
+    opts = if (selected.forall{|id| id != -1})
       ProductionBatch.find_all_by_status(:open).map{|batch| [batch.name, batch.id]}
     else
       [['(multiple)', -1]] + ProductionBatch.find_all_by_status(:open).map {|batch| [batch.name, batch.id]}
     end 
 
-    logger.info("Selected production batch is #{selected.inspect}")
+    logger.info("Selected production batch from #{opts.inspect} is #{selected.inspect}")
 
     options_for_select(opts, selected.orSome(nil))
   end
