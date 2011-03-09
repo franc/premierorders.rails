43851e05c9f1e1774755550376f1f647213c6e21
Kris Nuttycombe <kris.nuttycombe@gmail.com>
2011-03-09 16:59:25 -0700

    Bug fix for cutrite generation.

--- app/models/cutrite.rb
+++ app/models/cutrite.rb
@@ -37,39 +37,38 @@ module Cutrite
       job_item.item_name      
     ]
 
-    custom_attr_values = job_item.dvinci_color_code.cata(
-      lambda { |color_code|
-        panel_query = ItemQueries::ColorQuery.new('panel_material', job_item.dvinci_color_code) {|v| v.thickness(:in) != 0.25}
-        door_query = ItemQueries::ColorQuery.new('door_material', job_item.dvinci_color_code)
-        panel_material = Option.new(job_item.item).bind do |i| 
-          i.query(panel_query, []).orElseLazy{i.query(door_query, [])}
-        end
+    custom_attr_values = job_item.dvinci_color_code.map do |color_code|
+      panel_query = ItemQueries::ColorQuery.new('panel_material', color_code) {|v| v.thickness(:in) != 0.25}
+      door_query = ItemQueries::ColorQuery.new('door_material', color_code)
+      panel_material = Option.new(job_item.item).bind do |i| 
+        i.query(panel_query, []).orElseLazy{i.query(door_query, [])}
+      end
 
-        eb_query = ItemQueries::ColorQuery.new('edge_band', job_item.dvinci_color_code) {|v| v.width == 19 }
-        eb_material = Option.new(job_item.item).bind {|i| i.query(eb_query, [])}
+      eb_query = ItemQueries::ColorQuery.new('edge_band', color_code) {|v| v.width == 19 }
+      eb_material = Option.new(job_item.item).bind {|i| i.query(eb_query, [])}
 
-        eb2_query = ItemQueries::ColorQuery.new('edge_band', job_item.dvinci_color_code) {|v| v.width == 25 }
-        eb2_material = Option.new(job_item.item).bind {|i| i.query(eb2_query, [])}
+      eb2_query = ItemQueries::ColorQuery.new('edge_band', color_code) {|v| v.width == 25 }
+      eb2_material = Option.new(job_item.item).bind {|i| i.query(eb2_query, [])}
 
-        [
-          panel_material.map{|m| m.color}.orSome(''),
-          panel_material.map{|m| m.cutrite_code}.orSome(''),
-          eb_material.map{|m| m.cutrite_code}.orSome(''),
-          eb2_material.map{|m| m.cutrite_code}.orSome(''),
-          panel_material.map{|m| m.cutrite_code}.orSome(''),
-          eb_material.map{|m| m.cutrite_code}.orSome('')
-        ]
-      },
-      ['','','','','','']
+      [
+        panel_material.map{|m| m.color}.orSome(''),
+        panel_material.map{|m| m.cutrite_code}.orSome(''),
+        eb_material.map{|m| m.cutrite_code}.orSome(''),
+        eb2_material.map{|m| m.cutrite_code}.orSome(''),
+        panel_material.map{|m| m.cutrite_code}.orSome(''),
+        eb_material.map{|m| m.cutrite_code}.orSome('')
+      ]
+    end
+
+    item_data = (
+      basic_attr_values + 
+      custom_attr_values.orSome(['','','','','','']) + 
+      ["#{job_item.job.job_number}/#{job_item.tracking_id}", job_item.job.name] + 
+      job_item.job.cutrite_address_data
     )
-
-
-    (basic_attr_values + custom_attr_values + ["#{job_item.job.job_number}/#{job_item.tracking_id}", job_item.job.name] + job_item.job.cutrite_address_data).map do |v|
+    
+    item_data.map do |v| 
       v.to_s.gsub(/[,'"]/,'')
     end
   end
 end
-
-require 'property.rb'
-
-
