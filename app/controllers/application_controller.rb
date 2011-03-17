e9a01f32dead4b834f1ae91e2aa4511640ceff61
Kris Nuttycombe <kris.nuttycombe@gmail.com>
2011-03-17 16:15:56 -0600

    Fix for weird back button behavior in Chrome.
    https://www.pivotaltracker.com/story/show/10947003

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < ActionController::Base
   def manifest
     text = <<-MANIFEST
       CACHE MANIFEST
-      # version 1.0.3
+      # version 1.0.4
       /offline.html
       /catalog_orders
 
