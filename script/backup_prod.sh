b5b7577471a76103981d38c61191ef589ccc836b
Kris Nuttycombe <kris.nuttycombe@gmail.com>
2011-03-30 21:56:25 +0000

    Added drop commands to database backup to make testing easier.

--- script/backup_prod.sh
+++ script/backup_prod.sh
@@ -1,1 +1,1 @@
-pg_dump -U pg_orders -F p -w pg_orders > /var/www/premierorders.rails/db/history/pg_orders.backup
+pg_dump -U pg_orders -F p -c -w pg_orders > /var/www/premierorders.rails/db/history/pg_orders.backup
